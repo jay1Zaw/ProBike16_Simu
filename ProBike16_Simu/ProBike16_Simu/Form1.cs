0be2de3d7a652974b8ea2481d86f2ed3f5b22ac8
b3ki <rgrindau@gmx.de>
2016-05-03 15:25:26 +0200

    Oberfläche Sell_finished

--- ProBike16_Simu/ProBike16_Simu/Form1.cs
+++ ProBike16_Simu/ProBike16_Simu/Form1.cs
@@ -16,5 +16,125 @@ namespace ProBike16_Simu
         {
             InitializeComponent();
         }
+
+        private void Sell_Click(object sender, EventArgs e)
+        {
+            Sell_panel.Visible = true;
+        }
+
+        private void PPS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Purchase_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ManagementSummery_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Settings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown5_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void homeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startseiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Sell_panel.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Sell_panel.Visible = false;
+        }
+
+        private void pPSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void einkaufToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zusammenfassungManagementToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void einstellungenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p2_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p3_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_a_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p2_a_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p3_a_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_b_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p3_b_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_c_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p2_c_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p3_c_box_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
