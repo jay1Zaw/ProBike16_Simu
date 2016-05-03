c690c9fbaa195a6d9570c557907371a4cf0136aa
b3ki <rgrindau@gmx.de>
2016-05-03 13:41:51 +0200

    Reb_krasseOberfläche

--- ProBike16_Simu/ProBike16_Simu/Form1.cs
+++ ProBike16_Simu/ProBike16_Simu/Form1.cs
@@ -16,5 +16,102 @@ namespace ProBike16_Simu
         {
             InitializeComponent();
         }
+
+        private void Sell_Click(object sender, EventArgs e)
+        {
+            panel_Sell.Visible = true;
+            //panel_Sell.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel_Sell.Visible = false;
+            //panel_Sell.Enabled = false;
+        }
+
+        private void back_button_Click(object sender, EventArgs e)
+        {
+            panel_Sell.Visible = false;
+        }
+
+        private void PPS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Purchase_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ManagementSummery_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Settings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p2_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p3_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_a_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p2_a_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p3_a_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_b_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p2_b_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p3_b_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p1_c_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p2_c_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p3_c_textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
