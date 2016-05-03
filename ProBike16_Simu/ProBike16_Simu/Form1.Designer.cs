7191411d3ed1887b679316b0d8203c10bfa8b24e
b3ki <rgrindau@gmx.de>
2016-05-03 13:13:49 +0200

    Reb_Oberfläche

--- ProBike16_Simu/ProBike16_Simu/Form1.Designer.cs
+++ ProBike16_Simu/ProBike16_Simu/Form1.Designer.cs
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Sell = new System.Windows.Forms.Button();
+            this.PPS = new System.Windows.Forms.Button();
+            this.Purchase = new System.Windows.Forms.Button();
+            this.ManagementSummery = new System.Windows.Forms.Button();
+            this.Settings = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Sell
+            // 
+            this.Sell.Location = new System.Drawing.Point(38, 126);
+            this.Sell.Name = "Sell";
+            this.Sell.Size = new System.Drawing.Size(163, 68);
+            this.Sell.TabIndex = 0;
+            this.Sell.Text = "Vertrieb";
+            this.Sell.UseVisualStyleBackColor = true;
+            // 
+            // PPS
+            // 
+            this.PPS.Location = new System.Drawing.Point(242, 126);
+            this.PPS.Name = "PPS";
+            this.PPS.Size = new System.Drawing.Size(163, 68);
+            this.PPS.TabIndex = 1;
+            this.PPS.Text = "PPS";
+            this.PPS.UseVisualStyleBackColor = true;
+            // 
+            // Purchase
+            // 
+            this.Purchase.Location = new System.Drawing.Point(444, 126);
+            this.Purchase.Name = "Purchase";
+            this.Purchase.Size = new System.Drawing.Size(163, 68);
+            this.Purchase.TabIndex = 2;
+            this.Purchase.Text = "Einkauf";
+            this.Purchase.UseVisualStyleBackColor = true;
+            // 
+            // ManagementSummery
+            // 
+            this.ManagementSummery.Location = new System.Drawing.Point(38, 242);
+            this.ManagementSummery.Name = "ManagementSummery";
+            this.ManagementSummery.Size = new System.Drawing.Size(163, 68);
+            this.ManagementSummery.TabIndex = 3;
+            this.ManagementSummery.Text = "Zusammenfassung Management";
+            this.ManagementSummery.UseVisualStyleBackColor = true;
+            // 
+            // Settings
+            // 
+            this.Settings.Location = new System.Drawing.Point(242, 242);
+            this.Settings.Name = "Settings";
+            this.Settings.Size = new System.Drawing.Size(163, 68);
+            this.Settings.TabIndex = 4;
+            this.Settings.Text = "Einstellungen";
+            this.Settings.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(444, 242);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(163, 68);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(648, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.homeToolStripMenuItem.Text = "Start";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Hilfe";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "über...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(648, 376);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.Settings);
+            this.Controls.Add(this.ManagementSummery);
+            this.Controls.Add(this.Purchase);
+            this.Controls.Add(this.PPS);
+            this.Controls.Add(this.Sell);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "ProBike";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Sell;
+        private System.Windows.Forms.Button PPS;
+        private System.Windows.Forms.Button Purchase;
+        private System.Windows.Forms.Button ManagementSummery;
+        private System.Windows.Forms.Button Settings;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
