be6590ba43d508ba4db6a3c2ae0e1065c0e56021
jay1Zaw <jaymessage@live.de>
2016-05-03 15:54:53 +0200

    new classes

---
+++ ProBike16_Simu/ProBike16_Simu/Factory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBike16_Simu
+{
+    sealed class Factory
+    {
+        static readonly Factory instance = new Factory();
+
+        Storage storage = Storage.Instance;
+        List<Workplace> workplaces = new List<Workplace>();
+
+        public static Factory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private Factory()
+        {
+            initWorkplaces();
+        }
+
+        private void initWorkplaces()
+        {
+            for(uint i = 0; i < Constants.MAX_WORKPLACES; i++)
+                workplaces.Add(new Workplace(i, Constants.VARIABLE_MACHINE_COSTS[i], Constants.FIX_MACHINE_COSTS[i]));
+        }
+    }
+}
